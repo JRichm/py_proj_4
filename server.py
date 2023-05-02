01f4ed1a42654b349d522870431be0933a1d5228
JRichm <jblackenmusic@gmail.com>
2023-05-02 13:43:55 -0600

    added more files and connected basic front end

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from forms import TeamForm
+from model import connect_to_db
 
 app = Flask(__name__)
+app.secret_key = 'minecraft rocks'
 
 
         ##        Flask Routes        ##
 @app.route('/')
 def home():
-    return "Project Tracking App"
+    team_form = TeamForm()
+    return render_template('home.html', team_form=team_form)
 
+@app.route('/add_team', methods=['POST'])
+def add_team():
+    team_form = TeamForm()
+    
+    if team_form.validate_on_submit():
+        print(team_form.team_name.data)
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('home'))
 
         ##       Server Methods       ##
 if __name__ == '__main__':
+    connect_to_db(app)
     app.run(debug=True)
