14ee1d964aa2f2c6b44a8619cf8faa1d79e06358
JRichm <jblackenmusic@gmail.com>
2023-05-02 22:10:47 -0600

    m

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from forms import TeamForm, ProjectForm
-from model import connect_to_db, User, Teams, Projects
+from model import connect_to_db, User, Teams, Projects, db
 
 app = Flask(__name__)
 app.secret_key = 'minecraft rocks'
@@ -20,7 +20,10 @@ def add_team():
     team_form = TeamForm()
     
     if team_form.validate_on_submit():
-        print(team_form.team_name.data)
+        team_name = team_form.team_name.data
+        new_team = Teams(team_name, user_id)
+        db.session.add(new_team)
+        db.session.commit()
         return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
@@ -32,7 +35,15 @@ def add_project():
     project_form.update_teams(User.query.get(user_id).teams)
     
     if project_form.validate_on_submit():
-        print(project_form.project_name.data)
+        project_name = project_form.project_name.data
+        project_description = project_form.description.data
+        project_completed = project_form.completed.data
+        team_id = project_form.team_selection.data
+        
+        new_project = Projects(project_name, project_completed, team_id, description=project_description)
+        db.session.add(new_project)
+        db.session.commit()
+        
         return redirect(url_for('home'))
     else:
         return redirect(url_for('home'))
